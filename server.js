13db372556bb69455332bd505b3e27cf61375216
Samah Ali <44007303+Samhhali@users.noreply.github.com>
2021-03-04 06:08:44 +0200

    Get Providers & prooducts API

--- server.js
+++ server.js
@@ -13,3 +13,36 @@ app.listen(port)
 console.log('task API server started on: ' + port)
 
 
+// Get all providers
+app.get('/providers', (req,res)=> {
+  pool.getConnection((err,connection)=>{
+     if(err) throw err
+     console.log('connected as id '+ connection.threadId)
+     connection.query('SELECT* from providers', (err,rows)=> {
+         connection.release()
+
+         if(!err){
+              res.json(res.paginatedResult)
+         }else{
+             console.log(err)
+         }
+         console.log('the data from products are : \n', rows)
+     })
+  })
+})
+
+// Get Products in Specific category by CategoryID
+app.get('/products/:id', (req,res)=> {
+  pool.getConnection((err,connection)=>{
+     if(err) throw err
+     connection.query('SELECT* from products where CategoryID = ?',[req.params.id], (err,rows)=> {
+         connection.release()
+         if(!err){
+            res.json(res.paginatedResult)
+         }else{
+             console.log(err)
+         }
+     })
+  })
+})
+
