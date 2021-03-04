de3aee44cd5a000aaca499db49202bc5db0a6046
Samah Ali <44007303+Samhhali@users.noreply.github.com>
2021-03-04 06:12:49 +0200

    Create paginate function

--- server.js
+++ server.js
@@ -46,3 +46,35 @@ app.get('/products/:id', (req,res)=> {
   })
 })
 
+
+// paginate
+function paginate(model) {
+  return (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = {};
+    if (endIndex < model.length) {
+      result.next = {
+        page: page + 1,
+        limit: limit,
+      };  
+    }
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    try {
+      result.results =  model.slice(startIndex, endIndex);
+      res.paginatedResult = result;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
+}
+
